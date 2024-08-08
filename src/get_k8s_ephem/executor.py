4ca697d0a53979c8e79047724619d428aa555367
adam <athornton@lsst.org>
2024-08-08 10:13:30 -0700

    Modify image collection

--- src/get_k8s_ephem/executor.py
+++ src/get_k8s_ephem/executor.py
@@ -7,6 +7,9 @@ import subprocess
 from pathlib import Path
 from typing import Any
 
+IMAGESIZER_CTR = "docker.io/lsstsqre/imagesizer"
+IMAGESIZER_VER = "0.0.1"
+
 
 class Executor:
     """Executor assumes that `kubectl` is in your path and your kubeconfig
@@ -29,10 +32,22 @@ class Executor:
                     f"/api/v1/nodes/{node}/proxy/stats/summary",
                 ]
             )
-            nodejson = self._run_json(
-                ["kubectl", "get", "node", node, "-o", "json"]
+            image_text = self._run_text(
+                [
+                    "kubectl",
+                    "debug",
+                    "-it",
+                    f"node/{node}",
+                    "--image",
+                    f"{IMAGESIZER_CTR}:{IMAGESIZER_VER}",
+                    "--",
+                    "/bin/crictl",
+                    "-r",
+                    "unix:///host/run/containerd/containerd.sock",
+                    "images",
+                ]
             )
-            images = nodejson["status"]["images"]
+            images = self._parse_images(image_text)
             images.sort(key=lambda x: x["sizeBytes"], reverse=True)
             pods = stats["pods"]
             nodedata: dict[str, Any] = {}
@@ -46,6 +61,11 @@ class Executor:
                     nodedata["capacityBytes"] = es["capacityBytes"]
                     nodedata["podUsage"] = []
                     nodedata["images"] = images
+                    nodedata["imageCount"] = len(images)
+                    nodedata["totalImageSize"] = sum(
+                        [x["sizeBytes"] for x in images]
+                    )
+
                 nodedata["podUsage"].append(
                     {"pod": p_id, "usedBytes": es["usedBytes"]}
                 )
@@ -59,6 +79,35 @@ class Executor:
                 )
                 self._results.append(nodedata)
 
+    def _parse_images(self, image_text: str) -> list[dict[str, Any]]:
+        i_lines = image_text.split("\n")
+        i_lines = i_lines[2:]  # Discard pod-starting message and header
+        images = []
+        for li in i_lines:
+            if not li:
+                continue
+            (imagename, tag, imageid, size) = li.split()
+            if tag == "<none>":
+                tag = "latest"  # Docker default
+            fullname = f"{imagename}:{tag}"
+            i_size = self._parse_size(size)
+            images.append(
+                {"sizeBytes": i_size, "name": fullname, "id": imageid}
+            )
+        return images
+
+    def _parse_size(self, size_text: str) -> int:
+        if size_text.endswith("GB"):
+            sz_f = float(size_text[:-2])
+            return int(1e9 * sz_f)
+        elif size_text.endswith("MB"):
+            sz_f = float(size_text[:-2])
+            return int(1e6 * sz_f)
+        elif size_text.endswith("kB"):
+            sz_f = float(size_text[:-2])
+            return int(1e3 * sz_f)
+        return 0  # Smaller than a KB, or unparseable == "Doesn't count"
+
     def report(self) -> None:
         """Print the results."""
         res_str = json.dumps(self._results)
@@ -66,6 +115,10 @@ class Executor:
             self._file.write_text(res_str)
         else:
             print(res_str)  # noqa: T201
+
+    def _run_text(self, args: list[str]) -> str:
+        result = subprocess.run(args, capture_output=True, check=True)
+        return result.stdout.decode()
 
     def _run_json(self, args: list[str]) -> dict[str, Any]:
         result = subprocess.run(args, capture_output=True, check=True)
